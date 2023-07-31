5b9c93050979cf90aa64730839ab10999c07ea85
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-31 18:26:17 -0300

    Update test configuration

--- test/test.tf
+++ test/test.tf
@@ -1,22 +1,13 @@
 module "swarm" {
   source            = "./.."
-  base_name         = "rolling_stones"
+  base_name         = "astrix"
   is_production     = false
   instance_type     = "t2.micro"
-  base_domain       = var.base_domain
-  subdomain_label   = var.subdomain_label
-  deployment_config = var.deployment_config
-}
-
-variable "base_domain" {
-  type = string
-}
-
-variable "subdomain_label" {
-  type = string
-}
-
-variable "deployment_config" {
-  type      = map(string)
-  sensitive = true
+  base_domain       = local.base_domain
+  subdomain_label   = "astrix"
+  deployment_config = {
+    git_platform = "github"
+    git_url      = "https://github.com/adarlan/traefik-swarm-deployment.git"
+    git_ref      = "main"
+  }
 }
