f49c690628e571eecfcf9310c82bc0f72f242967
Adarlan Teixeira <adarlan18@gmail.com>
2023-07-31 18:29:11 -0300

    Format test.tf

--- test/test.tf
+++ test/test.tf
@@ -1,10 +1,10 @@
 module "swarm" {
-  source            = "./.."
-  base_name         = "astrix"
-  is_production     = false
-  instance_type     = "t2.micro"
-  base_domain       = local.base_domain
-  subdomain_label   = "astrix"
+  source          = "./.."
+  base_name       = "astrix"
+  is_production   = false
+  instance_type   = "t2.micro"
+  base_domain     = local.base_domain
+  subdomain_label = "astrix"
   deployment_config = {
     git_platform = "github"
     git_url      = "https://github.com/adarlan/traefik-swarm-deployment.git"
